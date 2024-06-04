9c815bd32a94560d5456cdfc767fe3ecd3c732c4
Microondas <143319861+microwavedd@users.noreply.github.com>
2024-06-04 16:04:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const balls = document.querySelectorAll('.ball');
 const gameArea = document.getElementById('gameArea');
+const scoreElement = document.getElementById('score');
+const highScoreElement = document.getElementById('highScore');
+let score = 0;
+let highScore = 0;
+let ballsClicked = 0;
+
 const gameAreaWidth = gameArea.offsetWidth;
 const gameAreaHeight = gameArea.offsetHeight;
 
 function getRandomPosition() {
-    const x = Math.random() * (gameAreaWidth - 50); // 50 is the ball width
-    const y = Math.random() * (gameAreaHeight - 50); // 50 is the ball height
+    const x = Math.random() * (gameAreaWidth - 50);
+    const y = Math.random() * (gameAreaHeight - 50);
     return { x, y };
 }
 
 function showBalls() {
+    ballsClicked = 0;
     balls.forEach(ball => {
+        ball.dataset.clicked = 'false';
         const { x, y } = getRandomPosition();
         ball.style.left = `${x}px`;
         ball.style.top = `${y}px`;
@@ -22,6 +30,11 @@ function hideBalls() {
     balls.forEach(ball => {
         ball.classList.add('null');
     });
+    if (ballsClicked < balls.length) {
+        resetScore();
+    } else {
+        updateScore();
+    }
 }
 
 function toggleBalls() {
@@ -33,7 +46,26 @@ function toggleBalls() {
 }
 
 function handleBallClick(event) {
-    event.target.classList.add('null');
+    if (event.target.dataset.clicked === 'false') {
+        event.target.dataset.clicked = 'true';
+        event.target.classList.add('null');
+        score += 50;
+        ballsClicked++;
+        updateScore();
+    }
+}
+
+function resetScore() {
+    if (score > highScore) {
+        highScore = score;
+        highScoreElement.textContent = highScore;
+    }
+    score = 0;
+    scoreElement.textContent = score;
+}
+
+function updateScore() {
+    scoreElement.textContent = score;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
